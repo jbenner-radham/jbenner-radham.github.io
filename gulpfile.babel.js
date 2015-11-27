dc2420aee02134f6061c2bde1d335f7cee18e6d5
James Benner <james.benner@gmail.com>
2015-11-26 21:02:11 -0500

    Check in before switching to Harp architecture.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,28 +7,36 @@ var dateTime    = require('@radioactivehamster/date-time');
 var fs          = require('fs');
 var gulp        = require('gulp');
 var htmltidy    = require('gulp-htmltidy');
+var jade        = require('gulp-jade');
 var yaml        = require('js-yaml');
 var less        = require('gulp-less');
 var pkg         = require('./package.json');
 var stachio     = require('gulp-stachio');
+var stylus      = require('gulp-stylus');
 
 gulp.task('serve', ['style', 'template'], () => {
     browserSync.init({
         open: false,
         server: { baseDir: './' }
     });
-    gulp.watch('./src/**/*.*', ['style', 'template']).on('change', browserSync.reload);
+    gulp.watch('./src/style/stylus/*.styl', ['style']);
+    gulp.watch('./src/**/*.*', ['template']).on('change', browserSync.reload);
 });
 
 gulp.task('style', () => {
-    return gulp.src('./src/style/main.less')
-        .pipe(less())
-        .pipe(csscomb())
+    //let stylesheet = './src/style/main.less';
+    let stylesheet = './src/style/stylus/main.styl';
+
+    return gulp.src(stylesheet)
+        .pipe(stylus())
+        //.pipe(less())
+        //.pipe(csscomb())
         /**
          * @todo Correct the color conflict with "links.css."
          */
         .pipe(colorguard().on('error', e => console.warn(e.message)))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.stream({match: '**/*.css'}));
 });
 
 gulp.task('template', () => {
@@ -39,9 +47,18 @@ gulp.task('template', () => {
     let author     = pkg.author.replace(/ <.+>/i, '');
     let cname      = fs.readFileSync('./CNAME').toString();
     let htmltidyrc = yaml.load(fs.readFileSync('./.htmltidyrc').toString());
+    //let templates  = './src/template/**/*.hbs';
+    let templates  = './src/template/jade/**/*.jade';
 
-    return gulp.src('./src/template/**/*.hbs')
-        .pipe(stachio({ author: author, cname: cname, timestamp: dateTime() }))
+    return gulp.src(templates)
+        //.pipe(stachio({ author: author, cname: cname, timestamp: dateTime() }))
+        .pipe(jade({
+            locals: {
+                author:    author,
+                cname:     cname,
+                timestamp: dateTime()
+            }
+        }))
         .pipe(htmltidy(htmltidyrc))
         .pipe(gulp.dest('./'));
 });
