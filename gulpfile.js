8b9086765f416e475313eb4501a75f1aa30f7cf7
James Benner <james.benner@gmail.com>
2016-04-03 16:47:27 -0400

    Refactor and complete redesign for `0.2.0`.

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,22 @@ const csscomb     = require('gulp-csscomb');
 const dateTime    = require('@radioactivehamster/date-time');
 const fs          = require('fs');
 const gulp        = require('gulp');
+const htmlhint    = require('gulp-htmlhint');
 const htmltidy    = require('gulp-htmltidy');
 const yaml        = require('js-yaml');
 const less        = require('gulp-less');
 const pkg         = require('./package.json');
 const stachio     = require('gulp-stachio');
+const taskDoc     = require('gulp-task-doc').patchGulp();
 
+/**
+ * Display this help.
+ */
+gulp.task('help', taskDoc.help());
+
+/**
+ * Start web server.
+ */
 gulp.task('serve', ['style', 'template'], () => {
     browserSync.init({
         open: false,
@@ -20,31 +30,37 @@ gulp.task('serve', ['style', 'template'], () => {
     gulp.watch('./src/**/*.*', ['style', 'template']).on('change', browserSync.reload);
 });
 
+/**
+ * Build stylesheets.
+ */
 gulp.task('style', () => {
     return gulp.src('./src/style/main.less')
         .pipe(less())
         .pipe(csscomb())
-        /**
-         * @todo Correct the color conflict with "links.css."
-         */
         .pipe(colorguard().on('error', e => console.error(e.message)))
         .pipe(gulp.dest('./'));
 });
 
+/**
+ * Compile templates.
+ */
 gulp.task('template', () => {
     /**
      * Remove angle bracket enclosed email addresses.
      * @todo Look into potential "safe string" encoding issues in `stachio`.
      */
-    let author     = pkg.author.replace(/ <.+>/i, '');
-    let cname      = fs.readFileSync('./CNAME').toString().trim();
-    let htmltidyrc = yaml.load(fs.readFileSync('./.htmltidyrc').toString());
-    let vcard      = fs.readFileSync('./data/me/vcard.jsonld').toString().trim();
+    let author = pkg.author.replace(/ <.+>/i, '');
+    let cname  = fs.readFileSync('./CNAME').toString().trim();
+    let tidyrc = yaml.load(fs.readFileSync('./.tidyrc').toString());
+    let vcard  = fs.readFileSync('./data/me/vcard.jsonld').toString().trim();
 
     return gulp.src('./src/template/**/*.hbs')
         .pipe(stachio({ author: author, cname: cname, timestamp: dateTime(), vcard: vcard }))
-        .pipe(htmltidy(htmltidyrc))
+        .pipe(htmltidy(tidyrc))
+        .pipe(htmlhint())
+        .pipe(htmlhint.reporter())
         .pipe(gulp.dest('./'));
 });
 
+// @internal
 gulp.task('default', ['style', 'template']);
