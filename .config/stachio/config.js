894f5b586a5ae84cc21262458fe26ac5cbfa9186
James Benner <james.benner@gmail.com>
2021-02-26 19:51:59 -0500

    Revised website

---
+++ .config/stachio/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    context: {
+        available: '2016-04-03T20:21:40Z',
+        cname: 'www.jamesbenner.com',
+        description: 'Developer, Computer Scientist, Researcher, and Geek',
+        timestamp: new Date().toISOString().replace(/\.[0-9]{3}Z$/, 'Z')
+    }
+};
---
+++ Makefile
@@ -0,0 +1,17 @@
+# vim: set noexpandtab:
+
+default: clean css html
+
+clean:
+	@rm index.css index.css.map index.html
+
+css:
+	@npx lessc --source-map src/style/index.less ./index.css
+
+html:
+	@stachio src/template/ .
+
+.PHONY: default \
+		clean \
+		css \
+		html
