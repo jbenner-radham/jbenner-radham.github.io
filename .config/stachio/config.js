7cf4bd7533d215ffdf1521b6e0fea8a4d77e6463
James Benner <james.benner@gmail.com>
2021-12-01 18:52:40 -0500

    Update description

--- .config/stachio/config.js
+++ .config/stachio/config.js
@@ -2,7 +2,7 @@ module.exports = {
     context: {
         available: '2016-04-03T20:21:40Z',
         cname: 'www.jamesbenner.com',
-        description: 'Developer, Computer Scientist, Researcher, and Geek',
+        description: 'Developer, Researcher, and Geek',
         timestamp: new Date().toISOString().replace(/\.[0-9]{3}Z$/, 'Z')
     }
 };
