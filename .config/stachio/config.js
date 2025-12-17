7179ae8a1ccfedb5c7d4839c5c0a84ba456be281
James Benner <hello@jamesbenner.com>
2025-12-16 21:14:17 -0500

    Update indent size

--- .config/stachio/config.js
+++ .config/stachio/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    context: {
-        available: '2016-04-03T20:21:40Z',
-        cname: 'www.jamesbenner.com',
-        description: 'Developer, Researcher, and Geek',
-        timestamp: new Date().toISOString().replace(/\.[0-9]{3}Z$/, 'Z')
-    }
+  context: {
+    available: '2016-04-03T20:21:40Z',
+    cname: 'www.jamesbenner.com',
+    description: 'Developer, Researcher, and Geek',
+    timestamp: new Date().toISOString().replace(/\.[0-9]{3}Z$/, 'Z')
+  }
 };
